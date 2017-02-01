edcf7e726ecbbd7b7e754dbb2450b94c15e3f693
kimdhoe <kimdhoe1@gmail.com>
2017-02-01 22:39:03 +0900

    setup project

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const { resolve } = require('path')
+const webpack = require('webpack')
+
+const config = {
+  context: resolve(__dirname, 'src'),
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './index.js',
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  devServer: {
+    hot: true,
+    contentBase: resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  devtool: 'cheap-eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader'
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
+}
+
+module.exports = config
