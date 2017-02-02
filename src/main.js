ae62459d42eb01d82f621c8e2df61b2b7021309a
kimdhoe <kimdhoe1@gmail.com>
2017-02-03 04:38:28 +0900

    configure production build

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux'
 
 import App from './app'
 import configureStore from './store'
-// import 'sanitize.css/sanitize.css'
 import './style/style.scss'
 
 const store = configureStore()
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,54 @@
 const { resolve } = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const { getIfUtils, removeEmpty } = require('webpack-config-utils')
 
-const config = {
-  context: resolve(__dirname, 'src'),
-  entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    './main.js',
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: resolve(__dirname, 'dist'),
-    publicPath: '/'
-  },
-  devServer: {
-    hot: true,
-    contentBase: resolve(__dirname, 'dist'),
-    publicPath: '/'
-  },
-  devtool: 'cheap-eval-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: [
-          'babel-loader'
-        ],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-      }
-    ]
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin()
-  ]
+const config = env => {
+  const { ifProd, ifNotProd } = getIfUtils(env)
+
+  return {
+    context: resolve(__dirname, 'src'),
+    entry: removeEmpty([
+      ifNotProd('react-hot-loader/patch'),
+      ifNotProd('webpack-dev-server/client?http://localhost:8080'),
+      ifNotProd('webpack/hot/only-dev-server'),
+      './main.js',
+    ]),
+    output: {
+      filename: ifProd('bundle.[chunkhash].js', 'bundle.js'),
+      path: resolve(__dirname, 'dist'),
+      publicPath: '/'
+    },
+    devServer: {
+      hot: true,
+      contentBase: resolve(__dirname, 'dist'),
+      publicPath: '/'
+    },
+    devtool: ifProd('cheap-module-source-map', 'cheap-eval-source-map'),
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: [ 'babel-loader' ],
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: ExtractTextPlugin.extract({
+            fallbackLoader: 'style-loader',
+            loader: [ 'css-loader', 'sass-loader' ]
+          })
+        }
+      ]
+    },
+    plugins: removeEmpty([
+      new HtmlWebpackPlugin({ template: 'index.html' }),
+      new ExtractTextPlugin('styles.css'),
+      ifNotProd(new webpack.HotModuleReplacementPlugin()),
+      ifNotProd(new webpack.NamedModulesPlugin())
+    ])
+  }
 }
 
 module.exports = config
